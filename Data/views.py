f3f2c4afc484db04a139238d631c6fd0d8e99983
leAyush98 <rod999lee@gmail.com>
2023-05-29 15:18:02 +0530

    auth done

--- Data/views.py
+++ Data/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .forms import BlogPostForm
 from .models import BlogPost
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
 def create_post(request):
     form = BlogPostForm()
     return render(request, "Data/create_post.html", {"form" : form})
--- People/views.py
+++ People/views.py
@@ -8,14 +8,31 @@ from .forms import RegisterUserForm
 # Create your views here.
 
 def register_user(request):
-    form = RegisterUserForm()
-    return render(request, "People/register.html", {"form" : form})
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            user = authenticate(request, username=username, password=password, email = email, first_name = first_name, last_name = last_name)
+            login(request, user)
+            messages.success(request, f"Registration Successful! Welcome {str(username).title()}!")
+            return redirect('home')
+        else:
+            messages.error(request, "There has been some error...")
+            return redirect('register')
+    else:
+        form = RegisterUserForm()
+        return render(request, "People/register.html", {"form" : form})
 
 def login_user(request):
     if request.method == "POST":
         user = authenticate(request, username = request.POST["username"], password = request.POST["password"])
         if user:
-            messages.success(request, f"Welcome {user}")
+            messages.success(request, f"Welcome {user.first_name}!")
             return redirect("home")
         else:
             messages.success(request, f"Please check your credentials.")
